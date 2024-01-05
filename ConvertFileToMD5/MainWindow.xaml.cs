c6edffa95463d170f5366927bdb868e0d2f55412
Windows <vtnduc2382000@gmail.com>
2024-01-05 19:00:46 +0700

    Add project files.

---
+++ ConvertFileToMD5/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Win32;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ConvertFileToMD5
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        string CalculateMD5(string filePath)
+        {
+            using (var md5 = MD5.Create())
+            {
+                using (var stream = File.OpenRead(filePath))
+                {
+                    byte[] hashBytes = md5.ComputeHash(stream);
+                    StringBuilder sb = new StringBuilder();
+
+                    for (int i = 0; i < hashBytes.Length; i++)
+                    {
+                        sb.Append(hashBytes[i].ToString("x2"));
+                    }
+
+                    return sb.ToString();
+                }
+            }
+        }
+
+        private void btnChooseFile_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            if(dialog.ShowDialog() == true)
+            {
+                txtFilePath.Text = dialog.FileName;
+            }
+        }
+
+        private void btnConvertFileToMD5_Click(object sender, RoutedEventArgs e)
+        {
+            txtMD5FromFile.Text=CalculateMD5(txtFilePath.Text);
+        }
+    }
+}
